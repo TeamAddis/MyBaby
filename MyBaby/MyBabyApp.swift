01bec96296a61c9f0211c30c103e4c15c35fe926
Andrew Addis <addisandrew1@gmail.com>
2022-01-20 17:26:06 +0900

    Initial Commit

---
+++ MyBaby/MyBabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBabyApp.swift
+//  MyBaby
+//
+//  Created by Andrew Addis on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct MyBabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
